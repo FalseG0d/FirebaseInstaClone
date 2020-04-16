ea82df33e50518e53d9aa31cee9c622453240640
Apoorv Garg <u.garg.10@gmail.com>
2020-04-16 14:56:36 +0530

    Commit

--- functions/index.js
+++ functions/index.js
@@ -1,12 +1,26 @@
 const functions = require('firebase-functions');
+
 const admin=require('firebase-admin');
 admin.initializeApp();
 
 const express=require("express");
 const app=express();
 
+const config={
+    apiKey: "AIzaSyCLbfgrTiEL4BAce3LMUHtPRwE9aY7BP1I",
+    authDomain: "core-falcon-267013.firebaseapp.com",
+    databaseURL: "https://core-falcon-267013.firebaseio.com",
+    projectId: "core-falcon-267013",
+    storageBucket: "core-falcon-267013.appspot.com",
+    messagingSenderId: "648579736082",
+    appId: "1:648579736082:web:6e1f49386c383c0dee6ecb",
+    measurementId: "G-VRYV5203RV"
+}
+
+const firebase=require("firebase");
+firebase.initializeApp(config);
 app.get('/screams',(request,response)=>{
-    admin.firestore().collection('screams').orderBy('createdBy','desc').get().then((data)=>{
+    admin.firestore().collection('screams').orderBy('createdAt','desc').get().then((data)=>{
         let screams=[];
         data.forEach((doc)=>{
             screams.push({
@@ -23,7 +37,7 @@ app.get('/screams',(request,response)=>{
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
 //
-
+app.post()
 app.post('/screams',(request,response)=>{
     
     const newScream={
